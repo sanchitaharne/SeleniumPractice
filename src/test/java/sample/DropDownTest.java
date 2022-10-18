572f13e240aaea57cadaded248a7b23f9c498fe2
sanchita <sanchita.241@gmail.com>
2022-10-19 00:58:27 +0530

    commit

--- src/test/java/sample/DropDownTest.java
+++ src/test/java/sample/DropDownTest.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class DropDownTest {
 
@@ -15,6 +19,8 @@ public class DropDownTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://formy-project.herokuapp.com/dropdown");
         driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("dropdownMenuButton")));
         WebElement dropdown = driver.findElement(By.id("dropdownMenuButton"));
         dropdown.click();
         WebElement autocompleteItem = driver.findElement(By.id("autocomplete"));
