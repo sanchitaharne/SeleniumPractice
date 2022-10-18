cb45a0ceb23e9801ca13d75963852e7a460ee810
sanchita <sanchita.241@gmail.com>
2022-10-19 00:54:12 +0530

    commit

--- src/test/java/sample/FIleUpload.java
+++ src/test/java/sample/FIleUpload.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.Time;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class FIleUpload {
     @Test
     public void fileUploadTest() throws InterruptedException {
@@ -13,6 +17,7 @@ public class FIleUpload {
         WebDriver driver = new ChromeDriver();
         driver.get("https://formy-project.herokuapp.com/fileupload");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement upload = driver.findElement(By.id("file-upload-field"));
         upload.sendKeys("file-to-upload.png");
     }
